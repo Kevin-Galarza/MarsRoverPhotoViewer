75dcdcb760d06a69b450b9fbe087f81f0755482c
Kevin Galarza <galarza.kevin@outlook.com>
2021-08-09 10:40:12 -0400

    Initial Commit

---
+++ MarsRoverPhotoViewer/MarsRoverPhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsRoverPhotoViewerApp.swift
+//  MarsRoverPhotoViewer
+//
+//  Created by Kevin Galarza on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MarsRoverPhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
